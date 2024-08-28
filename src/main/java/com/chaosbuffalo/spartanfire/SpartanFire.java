11791ece2e6dfd7b77a6a66ad441751ead2e33e4
fonnymunkey <fonnymunkey@gmail.com>
2024-08-28 01:43:07 -0400

    Bump version 1.3.0

--- src/main/java/com/chaosbuffalo/spartanfire/SpartanFire.java
+++ src/main/java/com/chaosbuffalo/spartanfire/SpartanFire.java
@@ -6,12 +6,12 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = SpartanFire.MODID, name = SpartanFire.NAME, version = SpartanFire.VERSION,
-        dependencies="required-after:iceandfire;required-after:spartanweaponry@[1.5.1,);required-after:llibrary@[1.7.19,)")
+        dependencies="required-after:iceandfire;required-after:spartanweaponry@[1.6.0,);required-after:llibrary@[1.7.19,)")
 public class SpartanFire
 {
     public static final String MODID = "spartanfire";
     public static final String NAME = "Spartan Fire";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.3.0";
 
     private static Logger logger;
 
