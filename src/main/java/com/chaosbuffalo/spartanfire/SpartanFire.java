4cb5f94b9dbe3896da68831ac4fa8f1896319826
fonnymunkey <fonnymunkey@gmail.com>
2025-01-12 16:19:27 -0500

    1.3.3 bump version

--- src/main/java/com/chaosbuffalo/spartanfire/SpartanFire.java
+++ src/main/java/com/chaosbuffalo/spartanfire/SpartanFire.java
@@ -13,7 +13,7 @@ public class SpartanFire
 {
     public static final String MODID = "spartanfire";
     public static final String NAME = "Spartan Fire";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
